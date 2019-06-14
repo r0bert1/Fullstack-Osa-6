fa239ce073fc5df042033bdf1813ae6b1ddf6b33
Robert <robert.pakkanen@helsinki.fi>
2019-06-14 22:16:00 +0300

    exercise 6.7

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,24 +1,6 @@
 import React from 'react'
-import { voteFor, createNote } from './reducers/anecdoteReducer'
-
-const AnecdoteForm = (props) => {
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    props.store.dispatch(
-      createNote(event.target.anecdote.value)
-    )
-  }
-
-  return (
-    <div>
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <input name="anecdote"/>
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
-}
+import AnecdoteForm from './components/AnecdoteForm'
+import { voteFor } from './reducers/anecdoteReducer'
 
 const App = (props) => {
   const anecdotes = props.store.getState()
